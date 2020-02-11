490d7b612cfd9026d8bb4cfbddad5aa96913309e
Agit Kulgu <agit.kulgu@hogarthww.com>
2020-02-11 12:35:57 +0300

    Material Ui import edildi ve componentler arası iletişim kuruldu

---
+++ multi-step-form/src/components/FormUserDetails.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+    render() {
+        const {values} = this.props;
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar title="Form User Details" />
+                    
+                </React.Fragment>
+            </MuiThemeProvider>
+        );
+    }
+}
+export default FormUserDetails
--- multi-step-form/src/components/UserForm.js
+++ multi-step-form/src/components/UserForm.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
+import FormUserDetails from './FormUserDetails';
 
 export class UserForm extends Component {
     state = {
-        step:1,
+        step: 1,
         firstName: '',
         lastName: '',
         email: '',
         accupation: '',
         city: '',
-        bio: '',
-    }
+        bio: ''
+    };
 
     // Step to next 
     nextStep = () => { 
@@ -27,10 +28,33 @@ export class UserForm extends Component {
         });
     }
 
+    // Handle fields Change
+    handleChange = input => e => { 
+        this.setState({[input]: e.target.value});
+    }
+
     render() {
-        return (
-            <div></div>
-        )
+        const {step} = this.state;
+        const {firstName, lastName, email, occupation, city, bio} = this.state;
+        const values = {firstName, lastName, email, occupation, city, bio};
+        
+        switch (step) {
+            case 1:
+            return (
+                <FormUserDetails 
+                    nextStep={this.nextStep}
+                    handleChange={this.handleChange}
+                    values={values}
+                />
+            );
+            case 2:
+                return <h1>FormPersonalDetails</h1>
+            case 3:
+                return <h1>Confirm</h1>
+            case 4:
+                return <h1>Success</h1>
+        }
+        
     }
 }
-export default UserForm
+export default UserForm;
